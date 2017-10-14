f0cb912fad22ac68c32c85e868d153bedc57078b
Thiago Galvani <thiagopaiva99@gmail.com>
2017-10-14 19:05:28 -0300

    modularizing the application and connecting to the database

--- backend/loader.js
+++ backend/loader.js
@@ -1,9 +1,2 @@
-const bodyParser = require('body-parser')
-const express    = require('express')
-const server     = express()
-const port       = 3003
-
-server.use( bodyParser.urlencoded({ extended: true }) )
-server.use( bodyParser.json() )
-
-server.listen(port, () => console.log( `Server running on port ${port}.` ))
+require('./config/server')
+require('./config/database')
